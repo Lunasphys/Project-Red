c0759a7fa7e95b790bdee5d0701990af1dcc95a4
Lunasphys <laurie.jeham@hotmail.fr>
2021-09-23 17:12:16 +0200

    perso.go

--- perso.go
+++ perso.go
@@ -5,14 +5,14 @@ import (
 )
 
 type personnage struct {
-	Nom  string
-	Classe string
-	Niveau int
-	Point_de_vie_max int
+	Nom                  string
+	Classe               string
+	Niveau               int
+	Point_de_vie_max     int
 	Point_de_vie_restant int
-	Inventaire []string
-	}
-	
+	Inventaire           []string
+}
+
 func (char *personnage) Init(Nom string, Classe string, Niveau int, Point_de_vie_max int, Point_de_vie_restant int, Inventaire []string) {
 	char.Nom = Nom
 	char.Classe = Classe
@@ -21,7 +21,7 @@ func (char *personnage) Init(Nom string, Classe string, Niveau int, Point_de_vie
 	char.Point_de_vie_restant = Point_de_vie_restant
 	char.Inventaire = Inventaire
 }
-	
+
 func main() {
 	var char1 personnage
 	char1.Init("Lunasphys", "Archer", 68, 1350, 1152, []string{"Potion", "Potion", "Potion"})
@@ -36,31 +36,27 @@ func (char1 *personnage) DisplayInfo() {
 	fmt.Println(char1.Niveau)
 	fmt.Println(char1.Point_de_vie_max)
 	fmt.Println(char1.Point_de_vie_restant)
-	fmt.Println(char1.Inventaire)	
+	fmt.Println(char1.Inventaire)
 }
-	
 
 func (char1 personnage) AccessInventory() {
 	if len(char1.Inventaire) == 0 {
-	fmt.Println("Votre inventaire est vide")
-	} 
+		fmt.Println("Votre inventaire est vide")
+	}
 	for i := 0; i < len(char1.Inventaire); i++ {
-	fmt.Println(char1.Inventaire[i])			
+		fmt.Println(char1.Inventaire[i])
 	}
-}	
-	
+}
+
 func (char1 *personnage) TakePot() {
-	for _,objets := range Inventaire {
-		if objects[] == potion {
-		fmt.Println("potion")
-			if (char1.Point_de_vie_actuel) < (char1.Point_de_vie_max) {
-			
-		
-		
-		if potion >= 1 {
-			if p
-		
-	if len(char1.Point_de_vie_actuel) >= (char1.Point_de_vie_max) {
-	fmt.Println(char1.Point_de_vie_max)
-	fmt.Println("Vous êtes full")
+	for _, objet := range char1.Inventaire {
+		if objet == "potion" {
+			if (char1.Point_de_vie_restant) < (char1.Point_de_vie_max) {
+				len(char1.Inventaire) -
+					fmt.Println("Vous utilisez une potion")
+			} else {
+				fmt.Println("Vous êtes full")
+			}
+		}
 	}
+}
