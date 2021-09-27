73bb731d70498d189ce4ca8df340c57862f794ce
Lunasphys <laurie.jeham@hotmail.fr>
2021-09-27 14:46:20 +0200

    poison

--- inventaire.go
+++ inventaire.go
@@ -1,6 +1,7 @@
 package main
 
-import "fmt"
+import ( "fmt" 
+"time" )
 
 func (char1 personnage) AccessInventory() {
 	if len(char1.Inventaire) == 0 {
@@ -33,21 +34,42 @@ func AddInventory(tab []string, s string) []string {
 
 func (char1 *personnage) TakePot() {
 	for _, objet := range char1.Inventaire {
-			if objet == "Potion" {
-				if (char1.Point_de_vie_restant) < (char1.Point_de_vie_max) {
-					char1.Inventaire = RemoveInventory(char1.Inventaire, objet)
-					char1.Point_de_vie_restant += 150
-					fmt.Println("Vous avez utilisé une potion")
-					if char1.Point_de_vie_restant >= char1.Point_de_vie_max {
-						char1.Point_de_vie_restant = char1.Point_de_vie_max
-						fmt.Println("Vous ne pouvez pas utiliser de potion")
-							if objet != "Potion" {
-							fmt.Println("Vous n'avez pas de potion")
+		if objet == "Potion" {
+			if (char1.Point_de_vie_restant) < (char1.Point_de_vie_max) {
+				char1.Inventaire = RemoveInventory(char1.Inventaire, objet)
+				char1.Point_de_vie_restant += 150
+				fmt.Println("Vous avez utilisé une potion")
+				if char1.Point_de_vie_restant >= char1.Point_de_vie_max {
+					char1.Point_de_vie_restant = char1.Point_de_vie_max
+					fmt.Println("Vous ne pouvez pas utiliser de potion")
+					if objet != "Potion" {
+						fmt.Println("Vous n'avez pas de potion")
 					}
 				}
 			}
+			break
 		}
 	}
 	char1.AccessInventory()
 	fmt.Println(char1.Point_de_vie_restant)
 }
+
+func (char1 *personnage) PoisonPot() {
+	for _, objet := range char1.Inventaire {
+		if objet == "Poison" {
+		fmt.Println("Applique un poison pendant 3 secondes")
+		char1.Point_de_vie_restant -= 10 
+		time.Sleep(3 * time.Second)
+		}
+	}	
+}	
+
+func (char1 *personnage) PoisonPot() {
+	for i := char1.Point_de_vie_restant; i -= 10; i-- {
+		if objet == "Poison" {
+		fmt.Println("Applique un poison pendant 3 secondes")
+		char1.Point_de_vie_restant -= 10 
+		time.Sleep(3 * time.Second)
+		}
+	}	
+}	
