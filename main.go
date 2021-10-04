8a822634c5471987c8f53b7a44ebea2e0ee6c942
Max <maxime-obry@hotmail.fr>
2021-10-04 11:28:37 +0200

    Test

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ type Marchand struct {
 func main() {
 	var char2 Marchand
 	var char1 personnage
-	char1.Init("Lunasphys", "Archer", 68, 1350, 1150, []string{"coup de poing"}, []string{"Potion de vie", "Potion de vie", "Potion de vie", "Potion de poison"}, 100)
-	char2.Init("Marchand", []string{"Potion de vie", "épée de fer", "Potion de poison", "Boule de feu", "Fourrure de loup", "Peau de troll", "Cuir de Sanglier", "Plume de Corbeau"}, 1000000)
+	char1.Init("Lunasphys", "Archer", 68, 1350, 1150, []string{"Coup de Poing"}, []string{"Potion de vie", "Potion de vie", "Potion de vie", "Potion de poison"}, 100)
+	char2.Init("Marchand", []string{"Potion de vie", "épée de fer", "Potion de poison", "Livre de sort : Boule de feu", "Fourrure de loup", "Peau de troll", "Cuir de Sanglier", "Plume de Corbeau"}, 1000000)
 	//char1.TakePot()
 	char1.menu(&Marchand{})
 }
