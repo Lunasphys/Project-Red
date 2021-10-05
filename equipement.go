290daa891697e4a9dd55c0e6ddf7cf400119e1c6
Lunasphys <laurie.jeham@hotmail.fr>
2021-10-05 11:11:45 +0200

    func equipement

--- equipement.go
+++ equipement.go
@@ -1,21 +1,42 @@
 package main
 
+import "fmt"
+
 func (char *equipement) Init(Tete string, Torse string, Bottes string) {
 	char.Tete = Tete
 	char.Torse = Torse
 	char.Bottes = Bottes
 }
 
-func AddEquip(tab []string, s string) []string {
-	if len(tab)-1 >= 2 {
-		fmt.Println("Vous avez déjà équipé un item")
-		return tab
+func (char1 *personnage) EquipHead(s string) string {
+	for _, tete := range char1.Inventaire {
+		if tete == s {
+			char1.Equipement.Tete = s
+			fmt.Println("Vous venez d'équiper : ", s)
+		}
 	}
-	return append(tab, s)
+	fmt.Println("Vous n'avez rien à équiper")
+	return char1.Equipement.Tete
 }
 
+func (char1 *personnage) EquipChest(s string) string {
+	for _, tete := range char1.Inventaire {
+		if tete == s {
+			char1.Equipement.Torse = s
+			fmt.Println("Vous venez d'équiper : ", s)
+		}
+	}
+	fmt.Println("Vous n'avez rien à équiper")
+	return char1.Equipement.Torse
+}
 
-func (char *equipement) EquipHead() {
-	for _, chapeau := range char1.Equipement {
-		if chapeau == "Chapeau de Sorcier" {
+func (char1 *personnage) EquipBoots(s string) string {
+	for _, tete := range char1.Inventaire {
+		if tete == s {
+			char1.Equipement.Bottes = s
+			fmt.Println("Vous venez d'équiper : ", s)
+		}
+	}
+	fmt.Println("Vous n'avez rien à équiper")
+	return char1.Equipement.Tete
 }
