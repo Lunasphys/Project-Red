dcfcfded4fe9bf243d0547dff06c88bfb094d4a0
Max <maxime-obry@hotmail.fr>
2021-10-05 14:33:40 +0200

    tache15

--- equipement.go
+++ equipement.go
@@ -8,14 +8,23 @@ func (char *equipement) Init(Tete string, Torse string, Bottes string) {
 	char.Bottes = Bottes
 }
 
+func (char1 *personnage) ShowEquipement() {
+	println(char1.Equipement.Tete)
+	println(char1.Equipement.Torse)
+	println(char1.Equipement.Bottes)
+}
+
 func (char1 *personnage) EquipHead(s string) string {
 	for _, tete := range char1.Inventaire {
 		if tete == s {
 			char1.Equipement.Tete = s
 			fmt.Println("Vous venez d'équiper : ", s)
+			char1.Point_de_vie_max += 10
 		}
 	}
-	fmt.Println("Vous n'avez rien à équiper")
+	if char1.Equipement.Tete != s {
+		fmt.Println("Vous n'avez rien à équiper")
+	}
 	return char1.Equipement.Tete
 }
 
@@ -24,6 +33,7 @@ func (char1 *personnage) EquipChest(s string) string {
 		if tete == s {
 			char1.Equipement.Torse = s
 			fmt.Println("Vous venez d'équiper : ", s)
+			char1.Point_de_vie_max += 25
 		}
 	}
 	fmt.Println("Vous n'avez rien à équiper")
@@ -35,8 +45,14 @@ func (char1 *personnage) EquipBoots(s string) string {
 		if tete == s {
 			char1.Equipement.Bottes = s
 			fmt.Println("Vous venez d'équiper : ", s)
+			char1.Point_de_vie_max += 15
 		}
 	}
 	fmt.Println("Vous n'avez rien à équiper")
+	return char1.Equipement.Tete
+}
+
+func (char1 *personnage) RemoveEquipementTete() string {
+
 	return char1.Equipement.Tete
 }
