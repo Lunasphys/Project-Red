f2939b540d71b5a5758b1d152904e44a6a6d35a5
Max <maxime-obry@hotmail.fr>
2021-10-15 15:18:22 +0200

    fin

--- forgeron.go
+++ forgeron.go
@@ -44,114 +44,48 @@ func menuCraft(char1 *personnage) { // Ce que le joueur peut forger comme item e
 	d := scanner.Text()
 	switch d {
 	case "1":
-		if len(char1.Inventaire) < 10 { // Voir si il y a assez de place dans l'inventaire
-			if char1.Argent >= 5 { // Verifie si le joueur a assez d'argent pour effectuer cette action
-				if char1.Craft(1, 1, "Plume de Corbeau", "Cuir de Sanglier") {
-					char1.Inventaire = char1.AddInventory(char1.Inventaire, chapeauAventurier) // Ajoute l'item dans l'inventaire
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Plume de Corbeau")   // Retire l'objet utilisé pour le craft de l'inventaire
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Cuir de Sanglier")   // Retire l'objet utilisé pour le craft de l'inventaire
-					char1.Argent -= 5
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de dépenser 5 pièces d'or")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de créer un Chapeau de l'aventurier")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				} else { // Indique au joueur qu'il n'a pas les objets nécessaires pour effectuer cette action
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous n'avez pas les ressources nécessaire pour créer cet objet")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Votre inventaire ne contient que les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-
-				}
-			} else { // Indique au joueur qu'il n'a pas assez de pièces d'or pour forger un item
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous êtes trop pauvre pour pouvoir forger un item")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous n'avez que :", char1.Argent, "pièces d'or")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			}
-		} else { // Indique au joueur qu'il a atteint la limite de son inventaire
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Vous n'avez pas assez de place dans votre inventaire")
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Votre inventaire contient les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-		}
+		char1.Making(5, 1, 1, "Plume de Corbeau", "Cuir de Sanglier", chapeauAventurier)
 	case "2":
-		if len(char1.Inventaire) < 10 { // Verifie l'inventaire
-			if char1.Argent >= 5 { // Verifie que le joueur a assez d'argent
-				if char1.Craft(2, 1, "Fourrure de Loup", "Peau de Troll") {
-					char1.Inventaire = char1.AddInventory(char1.Inventaire, "Tunique de l'aventurier") // Ajout de l'item forgé dans l'inventaire
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Fourrure de Loup")           // Enlève l'objet utilisé pour créer l'item demandé
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Fourrure de Loup")           // Enlève l'objet utilisé pour créer l'item demandé
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Peau de Troll")              // Enlève l'objet utilisé pour créer l'item demandé
-					char1.Argent -= 5
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de créer une Tunique de l'aventurier")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de dépenser 5 pièces d'or", "il vous reste :", char1.Argent, "pièces d'or")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Votre inventaire contient maintenant :", char1.Inventaire) // Renvoi l'inventaire du joueur
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				} else {
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous n'avez pas les ressources nécessaire pour créer cet objet")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Votre inventaire ne contient que les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				}
-			} else {
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous êtes trop pauvre pour pouvoir forger un item")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous n'avez que :", char1.Argent, "pièces d'or")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			}
-		} else {
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Vous n'avez pas assez de place dans votre inventaire")
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Votre inventaire contient les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-		}
+		char1.Making(5, 2, 1, "Fourrure de Loup", "Peau de Troll", tuniqueAventurier)
 	case "3":
-		if len(char1.Inventaire) < 10 { // Verifie si l'utilisateur a assez de place
-			if char1.Argent >= 5 {
-				if char1.Craft(1, 1, "Fourrure de Loup", "Cuir de Sanglier") {
-					char1.Inventaire = char1.AddInventory(char1.Inventaire, "Bottes de l'Aventurier") // Ajoute l'item forger dans l'inventaire
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Fourrure de Loup")          // Enlève l'objet utilisé pour créer l'item demandé
-					char1.Inventaire = RemoveInventory(char1.Inventaire, "Cuir de Sanglier")          // Enlève l'objet utilisé pour créer l'item demandé
-					char1.Argent -= 5
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de créer des Bottes de l'aventurier")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous venez de dépenser 5 pièces d'or", "il vous reste :", char1.Argent, "pièces d'or")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Votre inventaire contient maintenant les objets suivants:", char1.Inventaire) // Renvoi l'inventaire du joueur
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				} else {
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Vous n'avez pas les ressources nécessaire pour créer cet objet")
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-					fmt.Println("Votre inventaire contient les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-					fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				}
-			} else {
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous êtes trop pauvre pour pouvoir forger un item")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-				fmt.Println("Vous n'avez que :", char1.Argent, "pièces d'or")
-				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			}
-		} else {
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Vous n'avez pas assez de place dans votre inventaire")
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			fmt.Println("Votre invetaire contient les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
-			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-		}
+		char1.Making(5, 1, 1, "Fourrure de Loup", "Cuir de Sanglier", bottesAventurier)
 	case "4":
 		break
+	}
+}
+func (char1 *personnage) Making(argent1 int, nbobjet1 int, nbobjet2 int, objet1 string, objet2 string, equipement string) {
+	if len(char1.Inventaire) < char1.Tailleinventairemax { // Voir si il y a assez de place dans l'inventaire
+		if char1.Argent >= argent1 { // Verifie si le joueur a assez d'argent pour effectuer cette action
+			if char1.Craft(nbobjet1, nbobjet2, objet1, objet2) {
+				char1.Inventaire = char1.AddInventory(char1.Inventaire, equipement) // Ajoute l'item dans l'inventaire
+				char1.Inventaire = RemoveInventory(char1.Inventaire, objet1)        // Retire l'objet utilisé pour le craft de l'inventaire
+				char1.Inventaire = RemoveInventory(char1.Inventaire, objet2)        // Retire l'objet utilisé pour le craft de l'inventaire
+				char1.Argent -= argent1
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+				fmt.Println("Vous venez de dépenser", argent1, " d'or")
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+				fmt.Println("Vous venez de créer un ", equipement)
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+			} else { // Indique au joueur qu'il n'a pas les objets nécessaires pour effectuer cette action
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+				fmt.Println("Vous n'avez pas les ressources nécessaire pour créer cet objet")
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+				fmt.Println("Votre inventaire ne contient que les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
+				fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+			}
+		} else { // Indique au joueur qu'il n'a pas assez de pièces d'or pour forger un item
+			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+			fmt.Println("Vous êtes trop pauvre pour pouvoir forger un item")
+			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+			fmt.Println("Vous n'avez que :", char1.Argent, "pièces d'or")
+			fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+		}
+	} else { // Indique au joueur qu'il a atteint la limite de son inventaire
+		fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+		fmt.Println("Vous n'avez pas assez de place dans votre inventaire")
+		fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+		fmt.Println("Votre inventaire contient les objets suivants :", char1.Inventaire) // Renvoi l'inventaire du joueur
+		fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	}
 }
